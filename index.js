07e0fa2d185b6bc252b0adc249a4b5a85200439a
carmarq <carl.marq95@gmail.com>
2021-02-23 17:08:43 -0700

    added logical statements

---
+++ index.js
@@ -0,0 +1,23 @@
+// let hour = 19;
+// let isWeekend = false;
+// if  (hour < 7 || hour > 23 || isWeekend) {
+// 	document.write("Im sleep")
+// }
+
+// let rain = true;
+// let snow = false;
+// let sunny = true;
+
+// if (rain === true || snow === false && sunny === true  ) {
+//     document.write("it's okay to go outside");
+// }
+
+// let money = false;
+// let isBroke = false;
+// let nameSpace = "";
+// document.write( money || isBroke || nameSpace || "What??" );
+// Output = "What??";
+
+let x = 0;
+x === 0 || document.write("is true");
+false || document.write("not true");
